3877d918be8c4060eafdef2c5ca10536ece2f2e0
ChenYFan <53730587+ChenYFan@users.noreply.github.com>
2022-05-15 17:41:30 +0800

    Update _middleware.js

--- functions/api/_middleware.js
+++ functions/api/_middleware.js
@@ -9,5 +9,5 @@ export async function onRequest(context) {
     data, // arbitrary space for passing data between middlewares
   } = context;
 
-  return new Response("Hello, world!");
+  return fetch('https://blog.cyfan.top')
 }
