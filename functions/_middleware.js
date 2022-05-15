9a4ac9a84fda8f767293fcae60e9baad6c315de3
ChenYFan <53730587+ChenYFan@users.noreply.github.com>
2022-05-15 17:47:16 +0800

    Update and rename functions/api/_middleware.js to functions/_middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -9,5 +9,5 @@ export async function onRequest(context) {
     data, // arbitrary space for passing data between middlewares
   } = context;
 
-  return fetch('https://blog.cyfan.top')
+  return fetch('https://cdn.cyfan.top')
 }
